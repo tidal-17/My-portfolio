2293a5b91807fe559b0f5626703061e555c1ddf8
tidal-17 <vatsin.khakhar@gmail.com>
2026-08-10 15:53:06 +0200

    createdd 3D tiny cube

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Canvas } from '@react-three/fiber'
+
+function Box() {
+  return (
+    <mesh rotation={[0.4, 0.4, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <Box />
+    </Canvas>
+  )
+}
+export default App
